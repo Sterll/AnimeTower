0a02e0fc41a3ea47f6f2a7ce99b5f495e502f326
Sterll <sterllytb@gmail.com>
2023-06-13 00:27:11 +0200

    Initial commit

---
+++ src/main/java/fr/yanis/animetower/AnimeTower.java
@@ -0,0 +1,17 @@
+package fr.yanis.animetower;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnimeTower extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
